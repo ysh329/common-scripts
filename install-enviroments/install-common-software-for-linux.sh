e03e2bd64e52280313163698e887dd6232f5eed1
ysh329 <ysh329@sina.com>
2018-12-18 08:20:43 +0000

    update

--- install-enviroments/install-common-software-for-linux.sh
+++ install-enviroments/install-common-software-for-linux.sh
@@ -1,1 +1,14 @@
+sudo add-apt-repository ppa:fcitx-team/nightly
+
+sudo apt update
+
+# basic
 sudo apt install -y git tmux vim
+
+# pinyin
+sudo apt-get install fcitx
+sudo apt-get install fcitx-config-gtk
+sudo apt-get install fcitx-table-all
+sudo apt-get install im-switch
+# download dpkg from  http://pinyin.sogou.com/linux/?r=pinyin 
+# sudo dpkg -i ***.deb
